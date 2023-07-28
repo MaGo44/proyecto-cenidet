89e6089a57d4c3a12f62efccc5a66ba0760f66a4
Rafael Juarez <52475345+rjuarez10@users.noreply.github.com>
2023-07-28 18:06:22 +0000

    se crea página de aviso

--- pages/request-student-account/index.vue
+++ pages/request-student-account/index.vue
@@ -1,27 +1,30 @@
 <template>
-    <div style="height: 100%" class="row-style">
-        <div class="margin-row">
-            <v-row>
-                <v-col cols="12" sm="6">
-                    <v-card class="elevation-0">
-
-                    </v-card>
-
-                </v-col>
-            </v-row>
-        </div>
-
-    </div>
+  <div class="center-container">
+    <v-row class="center-content">
+      <v-col cols="12" sm="6">
+        <v-card class="card-style elevation-2">
+          <v-card-title>
+            <h4 class="mt-5 mb-3">
+              Favor de contactar con el coordinador de tu carrera para la creación de tu usuario y contraseña.
+            </h4>
+            <NuxtLink to="/login-student">
+              <v-col cols="12">
+                <span class="secondaryColor--text" style="cursor:pointer">Regresar</span>
+              </v-col>
+            </NuxtLink>
+          </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
-
 <style scoped>
-.margin-row {
-  margin: 0 5%;
-}
-
-.row-style {
-  background-color: #f8f7ff;
+.center-container {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .card-style {
@@ -30,9 +33,13 @@
 
 .center-content {
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100%;
 }
+
+.secondaryColor--text{
+color:#2CA58D;
+}
+
 </style>
